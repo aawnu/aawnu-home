291636afd5fbc587b93f357429e00e5dfead9bd1
AAW <aaw@aaw.nu>
2024-05-16 21:54:53 +0200

    fix: remove company from website

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,7 +2,6 @@
   import "../app.css";
   import PaperPlaneTilt from "phosphor-svelte/lib/PaperPlaneTilt";
   import GlobeX from "phosphor-svelte/lib/GlobeX";
-  import BuildingOffice from "phosphor-svelte/lib/BuildingOffice";
   import Copyright from "phosphor-svelte/lib/Copyright";
 </script>
 
@@ -15,7 +14,6 @@
     <div class="flex items-center justify-center gap-x-6 p-4 text-sm font-light">
       <div><Copyright weight="light" class="inline-block" /> 2024</div>
       <div><GlobeX weight="light" class="inline-block" /> Denmark</div>
-      <div><BuildingOffice weight="light" class="inline-block" /> SKABER</div>
       <div>
         <a class="text-brand-link" href="mailto:aaw@aaw.nu" rel="noopener noreferrer">
           <PaperPlaneTilt weight="light" class="inline-block" /> aaw@aaw.nu
