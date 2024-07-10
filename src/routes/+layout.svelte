a927c163e82b944d4bf00afde0d8fdf727b6c473
AAW <aaw@aaw.nu>
2024-07-10 11:37:31 +0200

    Add privacy policy

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,9 @@
 <script>
-  import "../app.css";
-  import PaperPlaneTilt from "phosphor-svelte/lib/PaperPlaneTilt";
-  import GlobeX from "phosphor-svelte/lib/GlobeX";
-  import Copyright from "phosphor-svelte/lib/Copyright";
+  import '../app.css'
+  import Keyhole from 'phosphor-svelte/lib/Keyhole'
+  import PaperPlaneTilt from 'phosphor-svelte/lib/PaperPlaneTilt'
+  import GlobeX from 'phosphor-svelte/lib/GlobeX'
+  import Copyright from 'phosphor-svelte/lib/Copyright'
 </script>
 
 <main class="min-h-[75vh]">
@@ -14,6 +15,11 @@
     <div class="flex items-center justify-center gap-x-6 p-4 text-sm font-light">
       <div><Copyright weight="light" class="inline-block" /> 2024</div>
       <div><GlobeX weight="light" class="inline-block" /> Denmark</div>
+      <div>
+        <a class="text-brand-link" href="/privacy" rel="noopener noreferrer">
+          <Keyhole weight="light" class="inline-block" /> Privacy
+        </a>
+      </div>
       <div>
         <a class="text-brand-link" href="mailto:aaw@aaw.nu" rel="noopener noreferrer">
           <PaperPlaneTilt weight="light" class="inline-block" /> aaw@aaw.nu
---
+++ src/routes/privacy/+page.svelte
@@ -0,0 +1,33 @@
+<script>
+  import ArrowUUpLeft from 'phosphor-svelte/lib/ArrowUUpLeft'
+</script>
+
+<svelte:head>
+  <title>AAW | Privacy</title>
+  <meta name="description" content="Your privacy when visiting AAW.nu" />
+  <meta property="og:url" content="https://aaw.nu/privacy" />
+  <meta property="og:site_name " content="AAW" />
+  <meta property="og:locale " content="en_DK" />
+  <meta property="og:title" content="Your privacy on AAW.nu" />
+  <meta property="og:description" content="Your privacy when visiting AAW.nu" />
+  <meta property="og:type" content="website" />
+</svelte:head>
+
+<div class="flex min-h-[75vh] items-center justify-center p-4">
+  <div class="container mx-auto w-full max-w-screen-sm">
+    <div class="mb-4">
+      <a class="text-xs text-brand-link" href="/"><ArrowUUpLeft weight="bold" class="mr-1 inline" />Home</a>
+    </div>
+    <h1 class="text-4xl font-bold leading-snug">Your Privacy</h1>
+
+    <p class="my-2 font-light leading-relaxed">This website does not collect/store data from visitors or store cookies.</p>
+
+    <h2 class="my-2 mt-6 text-2xl font-medium leading-snug">Your rights</h2>
+    <p class="my-2 font-light leading-relaxed">
+      You can submit a complaint to the danish data protection authorities, Datatilsynet, if you have concerns about how I process your personal information.
+      Find out more on <a class="font-normal text-brand-link" href="https://www.datatilsynet.dk/english" target="_blank" rel="noopener noreferrer">
+        datatilsynet.dk/english
+      </a>
+    </p>
+  </div>
+</div>
