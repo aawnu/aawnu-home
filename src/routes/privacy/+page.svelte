35fd1674f0911a3b95fdc0d785d764989c58501f
Alex Ahlgreen Westergaard <aaw@aaw.nu>
2024-08-13 19:27:32 +0200

    New website (#1)

--- src/routes/privacy/+page.svelte
+++ src/routes/privacy/+page.svelte
@@ -13,21 +13,16 @@
   <meta property="og:type" content="website" />
 </svelte:head>
 
-<div class="flex min-h-[75vh] items-center justify-center p-4">
-  <div class="container mx-auto w-full max-w-screen-sm">
-    <div class="mb-4">
-      <a class="text-xs text-brand-link" href="/"><ArrowUUpLeft weight="bold" class="mr-1 inline" />Home</a>
-    </div>
-    <h1 class="text-4xl font-bold leading-snug">Your Privacy</h1>
+<div class="container max-w-screen-sm space-y-2 py-24">
+  <h1 class="text-4xl font-black leading-snug">Your Privacy</h1>
 
-    <p class="my-2 font-light leading-relaxed">This website does not collect/store data from visitors or store cookies.</p>
+  <p class="leading-relaxed">This website does not collect/store data from visitors or store cookies.</p>
 
-    <h2 class="my-2 mt-6 text-2xl font-medium leading-snug">Your rights</h2>
-    <p class="my-2 font-light leading-relaxed">
-      You can submit a complaint to the danish data protection authorities, Datatilsynet, if you have concerns about how I process your personal information.
-      Find out more on <a class="font-normal text-brand-link" href="https://www.datatilsynet.dk/english" target="_blank" rel="noopener noreferrer">
-        datatilsynet.dk/english
-      </a>
-    </p>
-  </div>
+  <h2 class="!mt-6 text-2xl font-bold leading-snug">Your rights</h2>
+  <p class="leading-relaxed">
+    You can submit a complaint to the danish data protection authorities, Datatilsynet, if you have concerns about how I process your personal information. Find
+    out more on <a class="font-medium text-primary" href="https://www.datatilsynet.dk/english" target="_blank" rel="noopener noreferrer">
+      datatilsynet.dk/english
+    </a>
+  </p>
 </div>
---
+++ tailwind.config.ts
@@ -0,0 +1,81 @@
+import { fontFamily } from 'tailwindcss/defaultTheme'
+import type { Config } from 'tailwindcss'
+
+const config: Config = {
+  darkMode: ['class'],
+  content: ['./src/**/*.{html,js,svelte,ts}'],
+  safelist: ['dark'],
+  theme: {
+    container: {
+      center: true,
+      padding: '1rem',
+      screens: {
+        '2xl': '1400px',
+      },
+    },
+    extend: {
+      colors: {
+        border: 'hsl(0 0% 89.8% / <alpha-value>)',
+        input: 'hsl(0 0% 89.8% / <alpha-value>)',
+        ring: 'hsl(0 0% 3.9% / <alpha-value>)',
+        background: 'hsl(0 0% 100% / <alpha-value>)',
+        foreground: 'hsl(0 0% 3.9% / <alpha-value>)',
+        primary: {
+          DEFAULT: 'hsl(37 95% 55% / <alpha-value>)',
+          foreground: 'hsl(37 95% 5% / <alpha-value>)',
+          100: 'hsl(37 95% 92% / <alpha-value>)',
+          200: 'hsl(37 95% 80% / <alpha-value>)',
+          300: 'hsl(37 95% 67% / <alpha-value>)',
+          400: 'hsl(37 95% 55% / <alpha-value>)',
+          500: 'hsl(37 95% 42% / <alpha-value>)',
+          600: 'hsl(37 95% 30% / <alpha-value>)',
+          700: 'hsl(37 95% 17% / <alpha-value>)',
+          800: 'hsl(37 95% 5% / <alpha-value>)',
+        },
+        secondary: {
+          DEFAULT: 'hsl(337 95% 42% / <alpha-value>)',
+          foreground: 'hsl(337 95% 92% / <alpha-value>)',
+          100: 'hsl(337 95% 92% / <alpha-value>)',
+          200: 'hsl(337 95% 80% / <alpha-value>)',
+          300: 'hsl(337 95% 67% / <alpha-value>)',
+          400: 'hsl(337 95% 55% / <alpha-value>)',
+          500: 'hsl(337 95% 42% / <alpha-value>)',
+          600: 'hsl(337 95% 30% / <alpha-value>)',
+          700: 'hsl(337 95% 17% / <alpha-value>)',
+          800: 'hsl(337 95% 5% / <alpha-value>)',
+        },
+        destructive: {
+          DEFAULT: 'hsl(0 72.2% 50.6% / <alpha-value>)',
+          foreground: 'hsl(0 0% 98% / <alpha-value>)',
+        },
+        muted: {
+          DEFAULT: 'hsl(0 0% 96.1% / <alpha-value>)',
+          foreground: 'hsl(0 0% 45.1% / <alpha-value>)',
+        },
+        accent: {
+          DEFAULT: 'hsl(0 0% 96.1% / <alpha-value>)',
+          foreground: 'hsl(0 0% 9% / <alpha-value>)',
+        },
+        popover: {
+          DEFAULT: 'hsl(0 0% 100% / <alpha-value>)',
+          foreground: 'hsl(0 0% 3.9% / <alpha-value>)',
+        },
+        card: {
+          DEFAULT: 'hsl(0 0% 100% / <alpha-value>)',
+          foreground: 'hsl(0 0% 3.9% / <alpha-value>)',
+        },
+      },
+      borderRadius: {
+        lg: '0.5rem',
+        md: 'calc(0.5rem - 2px)',
+        sm: 'calc(0.5rem - 4px)',
+      },
+      fontFamily: {
+        sans: [...fontFamily.sans],
+        manrope: ['Manrope Variable', ...fontFamily.sans],
+      },
+    },
+  },
+}
+
+export default config
